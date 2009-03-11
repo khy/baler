4cbb1b69da43446be639e2cf69a3fdc0500a6b2f
Kevin Hyland <khy@me.com>
2009-03-10 22:08:43 -0400

    Made the basic spec more readable and concise

--- spec/basic_spec.rb
+++ spec/basic_spec.rb
@@ -3,58 +3,39 @@ require File.dirname(__FILE__) + '/spec_helper'
 class BasicGame
   include Baler
   
-  attr_accessor :date, :home_team, :visiting_team,
-    :home_score, :visiting_score, :mvp
+  attr_accessor :date, :home_team, :home_score, :mvp
     
   set_remote_source File.dirname(__FILE__) + '/samples/game.html' do |source|
     source.uses :hpricot
     
     source.map :date => 'html > body > ol > li > span.date'
     source.map :home_team => 'html > body > ol > li > span.team.home'
-    source.map :home_score => 'html > body > ol > li > span.score.home'
-    source.map :mvp => 'html > body > ol > li.nonexistant_class'
+    source.map :home_score => 'html > body > ol > li.nonexistant_class'
   end
 end
 
-describe 'basic Baler setup' do
+describe 'basic Baler functionality' do
   before(:each) do
     @game = BasicGame.new
   end
-  
-  describe 'after initialization' do
-    it 'should have no attributes with a value' do
-      @game.date.should be_nil
-      @game.home_team.should be_nil
-      @game.home_score.should be_nil
-      @game.mvp.should be_nil
-    end
-  end
-  
-  describe '#date' do
-    it 'should have the mapped data after it is gathered' do
+
+  describe '#gather' do
+    it 'should assign the data mapped to the attributes, where it exists' do
       @game.gather
       @game.date.should == 'Tuesday, February 3rd'
-    end
-  end
-
-  describe '#home_team' do
-    it 'should have the mapped data after it is gathered' do
-      @game.gather
       @game.home_team.should == 'Los Angeles Lakers'
     end
-  end
-
-  describe '#home_score' do
-    it 'should have the mapped data after it is gathered' do
+    
+    it 'should assign nil to attributes mapped to nonexistant data' do
+      @game.home_score = 112
       @game.gather
-      @game.home_score.should == '112'
+      @game.home_score.should be_nil
     end
-  end
-
-  describe '#mvp' do
-    it 'should have no value after it is gathered (since its mapped element is non-existant)' do
+    
+    it 'shouldn\'t have any affect on unmapped attributes' do
+      @game.mvp = 'Dwight Howard'
       @game.gather
-      @game.mvp.should be_nil
+      @game.mvp.should == 'Dwight Howard'
     end
   end
 end
