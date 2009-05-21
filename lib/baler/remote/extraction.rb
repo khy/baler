c8512818b24e9c746ccf5c7499e8ebf0a794ba32
Kevin Hyland <khy@me.com>
2009-05-21 00:25:06 -0400

    Added the option to pass the instance to a #map block (with any already-mapped attributes in place)

--- lib/baler/remote/extraction.rb
+++ lib/baler/remote/extraction.rb
@@ -1,15 +1,14 @@
 module Baler
   module Remote
     class Extraction
-      attr_accessor :document, :use_context
+      attr_accessor :document, :use_context, :block
       attr_reader :path
-      attr_writer :block
       
       DEFAULT_BLOCK = Proc.new{|elements| elements.inner_html}
 
       def initialize(path, block = nil, use_context = true)
         self.path = path
-        @block = block
+        @block = block || DEFAULT_BLOCK
         @use_context = use_context
       end
       
@@ -17,15 +16,11 @@ module Baler
         @path = path.squeeze(' ').strip
       end
 
-      def block
-        @block || DEFAULT_BLOCK
-      end
-
       alias use_context? use_context
       
-      def value(document, index = nil, index_absolute_elements = false)
+      def value(document, instance, index = nil, index_absolute_elements = false)
         elements = elements(document, index, index_absolute_elements)
-        value = self.block.call(elements) unless elements.empty?
+        value = block_value(elements, instance)
 
         if value.is_a? Parser::Element::Array
           value = value.length <= 1 ? value.first : value.to_array
@@ -39,6 +34,16 @@ module Baler
       end
 
       protected
+        def block_value(elements, instance)
+          unless elements.empty?
+            case @block.arity
+            when 1: @block.call(elements)
+            when 2: @block.call(elements, instance)
+            else    nil
+            end
+          end
+        end
+
         def elements(document, index = nil, index_absolute_elements = false)
           absolute_index = index if index_absolute_elements
           use_context? ?
---
+++ spec/advanced_block_spec.rb
@@ -0,0 +1,29 @@
+require File.dirname(__FILE__) + '/spec_helper'
+
+class AdvancedBlockGame
+  include Baler
+  
+  attr_accessor :year, :date
+    
+  set_remote_source GAME_PATH do |source|
+    source.set_context 'html > body > ol > li'
+    
+    source.map :year => 'h1.global.season', :context => false
+    
+    source.map  :date => 'span.date' do |result, game|
+      result.first.inner_html + ' ' + game.year
+    end
+  end
+end
+
+describe 'Baler advanced block functionality' do
+  before(:each) do
+    @game = AdvancedBlockGame.new
+  end
+  
+  describe '#gather' do
+    it 'should provide the instance to the map block if specified' do
+      @game.gather(0).date.should == "Tuesday, February 3rd 2008-09"
+    end
+  end
+end
