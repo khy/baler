f90427850d083781c88008497471a356525800ae
Kevin Hyland <khy@me.com>
2009-05-26 21:32:45 -0400

    First iteration for documentation

--- lib/baler/base.rb
+++ lib/baler/base.rb
@@ -1,26 +1,49 @@
 module Baler
   module Base
     module ClassMethods
+      # Adds a Remote::Source instance to the class for the supplied URL.
+      # A Remote::Source::Builder instance is yielded to the block.
       def set_remote_source(url)
         remote_source = Baler::Remote::Source.new(self, url)
         yield remote_source.builder if block_given?
         instance_variable_set(:@remote_source, remote_source)
       end
       
+      # Returns the Remote::Source instance built by <tt>#set_remote_source</tt>
       def remote_source
         instance_variable_get(:@remote_source)
       end
       
+      # Returns an array containing an instance of the parent class for each 
+      # context element in the remote document. Each instance's attributes are 
+      # gathered according to its array index.
+      #
+      # Options:
+      # [<tt>:url_mapping</tt>]  Used to resolve the document's URL before gathering.
       def build(options = {})
         remote_source.build(options)
       end
       
+      # _This method is only available for classes that use a recognized ORM_. 
+      #
+      # Functions similarly to <tt>#build</tt>, except that if an instance can
+      # be loaded from the database using the defined lookup attributes, it will be.
+      #
+      # Options:
+      # [<tt>:url_mapping</tt>]  Used to resolve the document's URL before gathering.
       def build_or_update(options = {})
         remote_source.build_or_update(options)
       end
     end
     
-    module InstanceMethods      
+    module InstanceMethods
+      # Applies each mapping in the remote source to the instance.
+      #
+      # Options:
+      # [<tt>:index</tt>]       Specifies which context element will be referenced.
+      # [<tt>:url_mapping</tt>] Used to resolve the document's URL before gathering.
+      # [<tt>:attributes</tt>]  Specifies a subset of instance attributes to gather.
+      # [<tt>:force</tt>]       When <tt>true</tt>, will ignore any gather conditions.
       def gather(options = {})
         self.class.remote_source.gather(self, options)
       end
--- lib/baler/remote/source/builder.rb
+++ lib/baler/remote/source/builder.rb
@@ -10,6 +10,29 @@ module Baler
           :use_context => true
         }
 
+        # Creates a mapping for the remote source. 
+        #
+        # There are two forms. The general form is
+        #
+        #   source.map 'html > body > h1' do |result, instance|
+        #     ...
+        #   end
+        #
+        # where the value of the css selector in the document along with the
+        # instance is yielded to the block. The more concise form is
+        #
+        #   source.map :post_title => 'html > body > h1' do |result|
+        #     ...
+        #   end
+        #
+        # where the value of the CSS selector is yielded to the block on its own.
+        # In the example, the result of the block will be passed to <tt>#post_title=</tt>
+        # at gather time.
+        #
+        # Options:
+        #
+        # [<tt>:use_context</tt>] When <tt>false</tt>, the selector will not be taken
+        #                         relative to the context. Defaults to <tt>true</tt>
         def map(*args, &block)
           mappings = args.extract_options
           options = mappings.extract_with_defaults!(DEFAULT_MAP_OPTIONS)
@@ -25,22 +48,37 @@ module Baler
           @source
         end
 
+        # Adds a gather condition that must evaluate to <tt>true</tt> in order for <tt>#gather</tt>
+        # to execute. A condition can either be an instance corresponding to an instance method, 
+        # or a block.
         def gather_if(*methods, &block)
           add_gather_conditions(methods, block, true)
         end
   
+        # Adds a gather condition that must evaluate to <tt>false</tt> in order for <tt>#gather</tt>
+        # to execute. A condition can either be an instance corresponding to an instance method, 
+        # or a block.
         def gather_unless(*methods, &block)
           add_gather_conditions(methods, block, false)
         end
   
+        # Defines a context element using a CSS selector. Mappings will be extracted relative to
+        # the the context, be default. Also, <tt>#build</tt> will return an array with a size equal
+        # to the number of context elements in a document.
+        #
+        # A context should correspond to a single instance of the master class. So, if BlogPost is the
+        # master class, a context element should contain all of the attributes for a single post.
         def set_context(path)
           @source.context_path = path
         end
         
+        # Defines the attributes that will be used by <tt>#build_or_update</tt> to lookup class
+        # instances using the ORM
         def set_lookup_attributes(*attributes)
           @source.lookup_attributes = attributes
         end
 
+        # Defines which parser Baler will use
         def uses(parser_type)
           @source.parser.type = parser_type
         end
