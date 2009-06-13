704249071e7a95d4735df72f897d2746ddca4658
Kevin Hyland <khy@me.com>
2009-06-12 20:24:25 -0400

    Removing needless complication from GatherCondition

--- lib/baler/remote/gather_condition.rb
+++ lib/baler/remote/gather_condition.rb
@@ -3,11 +3,11 @@ module Baler
     class GatherCondition
       def initialize(object, expected_value)
         @object = object
-        @expected_value = !!(expected_value)
+        @expected_value = expected_value
       end
 
       def met?(instance)
-        !!(return_value(instance)) == @expected_value
+        return_value(instance) == @expected_value
       end
       
       private
